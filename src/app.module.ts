22735ba00891441e63214863bdb898f105dc7131
felipe <aronakamoto@outlook.com>
2020-11-01 14:05:51 -0300

    Fix: Database username env

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { RoleMiddleware } from './middlewares/check.role';
     type: 'postgres',
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT || "", 10),
-    username: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD, 
     database: process.env.DATABASE_NAME,
     entities: [User],
