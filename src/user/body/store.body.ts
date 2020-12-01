6d5b48ac90e5041e31012d60abc44fc17dd60eb8
felipe <aronakamoto@outlook.com>
2020-12-01 18:26:21 -0300

    Feat: Configuration to run Swagger

--- src/user/body/store.body.ts
+++ src/user/body/store.body.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { UserRole } from "../user.model";
 
 export class StoreOneUser {
+    @ApiProperty()
     email: string;
+    @ApiProperty()
     password: string;
+    @ApiProperty()
     role: UserRole;
 }
